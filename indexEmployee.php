63c53d5fce9705178597f82ab368a749f6846b6b
Cyrielle Meyrueix <cmeyrueix44@gmail.com>
2019-06-17 16:52:02 -0700

    More formatting

--- indexEmployee.php
+++ indexEmployee.php
@@ -5,7 +5,7 @@
 <div>
   <ul>
     <li>
-      <img src="https://image.flaticon.com/icons/svg/1665/1665731.svg">
+      <img src="https://image.flaticon.com/icons/svg/1665/1665746.svg">
       <h3> <a href="createEmployee.php"><strong>Manage Employees</strong></a> </h3>
        <p>Add, Update, View, and Delete Employee</p>
     </li>
@@ -15,12 +15,12 @@
       <p>Add, Update, View, and Delete Salaries</p>
     </li>
     <li>
-      <img src="https://image.flaticon.com/icons/svg/1665/1665753.svg">
+      <img src="https://image.flaticon.com/icons/svg/1665/1665746.svg">
       <h3><a href="createMaintenance.php"><strong>Manage Housekeeping</strong></a></h3>
       <p>Add, Update, View, and Delete Housekeeping and Maintenance Records</p>
       </li>
     <li>
-      <img src="https://image.flaticon.com/icons/svg/1831/1831192.svg" >
+      <img src="https://image.flaticon.com/icons/svg/1665/1665746.svg" >
       <h3><a href="createLifeGuard.php"><strong>Manage Lifeguards and Instructors</strong></a></h3>
       <p>Add, Update, View, and Delete Lifeguards/Instructors</p>
     </li>
